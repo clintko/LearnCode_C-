fe30f1f10c029872aaf2427c48110ae129a57c2d
clintko <clint00724@gmail.com>
2016-05-17 17:20:18 +0800

    Module05 lecture code: introduction to OOP

---
+++ Edx_ProgramCSharp/code_mod05_OOP/DrinksMachine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod05_OOP
+{
+    class DrinksMachine
+    {
+        //public string location;
+        //public string make;
+        //public string model;
+
+        private string location;
+        private string make;
+        private string model;
+
+        // constructor
+        public DrinksMachine(string loc, string make, string model)
+        {
+            this.Location = loc;
+            this.Make = make;
+            this.Model = model; 
+
+        } // end constructor
+
+        // property of private variable location
+        public string Location
+        {
+            get { return location;  }
+            set { location = value; }
+        }
+
+        public string Make
+        {
+            get { return make;  }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model;  } 
+            set { model = value; }
+        }
+
+        public void MakeCappuccino()
+        {
+            Console.WriteLine("Cappuccino is made");
+        } // end method MakeCappuccino
+
+        public void MakeExpresso()
+        {
+            Console.WriteLine("Expresso is made");
+        } // end method MakeExpresso
+    } // end class DrinksMachine
+} // end namespace Mod05_OOP
---
+++ Edx_ProgramCSharp/code_mod05_OOP/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod05_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Module05_OOP");
+
+            // Video 01
+            //DrinksMachine myMachine = new DrinksMachine();
+            //myMachine.location = "Kitchen";
+            //myMachine.model = "DM1000";
+
+            //Console.WriteLine("Location: " + myMachine.location);
+            //Console.WriteLine("Make:     " + myMachine.make);
+            //Console.WriteLine("Model:    " + myMachine.model);
+
+            // Video02 Encapsulation
+            // assign values in constructor
+            DrinksMachine myMachine = new DrinksMachine("Kitchen", "Brand", "DM1000");
+
+            // get private value from properties
+            Console.WriteLine("Location: " + myMachine.Location);
+            Console.WriteLine("Make:     " + myMachine.Make);
+            Console.WriteLine("Model:    " + myMachine.Model);
+
+            // call method
+            myMachine.MakeCappuccino();
+            myMachine.MakeExpresso();
+            
+        } // end method main
+    } // end class
+} // end namespace Mod05_OOP
